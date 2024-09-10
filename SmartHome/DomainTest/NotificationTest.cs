d67b7a62603a662733aae6acb745bf6c66238e5f
Angelina Maverino <angelinamaverino16@gmail.com>
2024-09-10 18:56:16 -0300

    test: notification (red)

---
+++ SmartHome/DomainTest/NotificationTest.cs
@@ -0,0 +1,30 @@
+namespace DomainTest;
+
+[TestClass]
+public class NotificationTest
+{
+    private string _event = "Door Opened";
+    private DateTime _createdAt = DateTime.Today;
+    private bool _read = false;
+    private DateTime _readAt = DateTime.MinValue;
+
+    private Notification _notification; 
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _notification = new Notification(_event,_createdAt,_read,_readAt);
+    }
+    [TestMethod]
+    public void CreateNewNotification()
+    {
+        // Arrange
+        Notification notification = new Notification(_event,_createdAt,_read,_readAt);
+
+        // Assert
+        Assert.AreEqual(_event, notification.Event);
+        Assert.AreEqual(_read, notification.Read);
+        Assert.AreEqual(_createdAt, notification.CreatedAt);
+        Assert.AreEqual(_readAt, notification.ReadAt);
+    }
+}
