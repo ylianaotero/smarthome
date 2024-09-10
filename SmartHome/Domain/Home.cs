4dac1ef2f23edd358f28d2e2b9881f86e8bdcf6e
Angelina Maverino <angelinamaverino16@gmail.com>
2024-09-10 20:33:27 -0300

    test: new Home (green and refactor)

---
+++ SmartHome/Domain/Home.cs
@@ -0,0 +1,18 @@
+namespace Domain;
+
+public class Home
+{
+    public string Street { get; set; }
+    public int DoorNumber { get; set; }
+    public int Latitude { get; set; }
+    public int Longitude { get; set; }
+    
+    public Home(string street, int doorNumber,int latitude,int longitude)
+    {
+        Street = street;
+        DoorNumber = doorNumber;
+        Latitude = latitude;
+        Longitude = longitude; 
+    }
+
+}
--- SmartHome/DomainTest/HomeTest.cs
+++ SmartHome/DomainTest/HomeTest.cs
@@ -1,3 +1,5 @@
+using Domain;
+
 namespace DomainTest;
 
 [TestClass]
