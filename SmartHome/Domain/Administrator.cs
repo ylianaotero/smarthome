bcfcd0614e7203631ff35e43b25e35b7ce9e9049
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-09-11 18:08:36 -0300

    [GREEN] administrator password

--- SmartHome/Domain/Administrator.cs
+++ SmartHome/Domain/Administrator.cs
@@ -5,4 +5,5 @@ public class Administrator
     public string Name { get; set; }
     public string Surname { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
 }
