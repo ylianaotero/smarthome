05a46b034b8b9355ad8d31bacf5087455c637e46
Yliana Otero <ylianaotero@gmail.com>
2024-09-12 20:11:22 -0300

    add: PhotoURLs property in IDevice - REFACTOR

--- SmartHome/IDomain/IDevice.cs
+++ SmartHome/IDomain/IDevice.cs
@@ -4,5 +4,6 @@ public interface IDevice
 {
     public string Name { get; set; }
     public long Model { get; set; }
+    public List<string> PhotoURLs { get; set; }
     public bool IsConnected { get; set; }
 }
