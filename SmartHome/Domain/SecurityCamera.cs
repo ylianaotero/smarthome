88ac1412e75cdba48437cae5b2d2d1920b68e0d6
Yliana Otero <ylianaotero@gmail.com>
2024-09-12 20:10:58 -0300

    add: PhotoURLs property in SecurityCamera - GREEN

--- SmartHome/Domain/SecurityCamera.cs
+++ SmartHome/Domain/SecurityCamera.cs
@@ -6,5 +6,6 @@ public class SecurityCamera : IDevice
 {
     public string Name { get; set; }
     public long Model { get; set; }
+    public List<string> PhotoURLs { get; set; }
     public bool IsConnected { get; set; }
 }
