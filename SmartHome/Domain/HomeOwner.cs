529917e80ba89c96bb094c013a7bca489d661ad0
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-09-11 22:59:41 -0300

    [RED] companyOwner class

--- SmartHome/Domain/HomeOwner.cs
+++ SmartHome/Domain/HomeOwner.cs
@@ -8,4 +8,5 @@ public class HomeOwner : IRole
     public string Surname { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    public string ProfilePhoto { get; set; }
 }
---
+++ SmartHome/DomainTest/CompanyOwnerTest.cs
@@ -0,0 +1,18 @@
+namespace DomainTest;
+
+[TestClass]
+public class CompanyOwnerTest
+{
+    [TestMethod]
+    public void TestAddNameToCompanyOwner()
+    {
+        // Arrange
+        CompanyOwner companyOwner = new CompanyOwner();
+        
+        // Act
+        companyOwner.Name = "Francisco";
+
+        // Assert
+        Assert.AreEqual("Francisco", companyOwner.Name);
+    }
+}
