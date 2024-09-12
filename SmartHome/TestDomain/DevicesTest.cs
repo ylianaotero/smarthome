773f3f3f462a22a01bfac42db51eb5f4bbc9bc40
Yliana Otero <ylianaotero@gmail.com>
2024-09-12 20:40:42 -0300

    fix: cleanup code - REFACTOR

--- SmartHome/TestDomain/DevicesTest.cs
+++ SmartHome/TestDomain/DevicesTest.cs
@@ -11,60 +11,47 @@ public class DevicesTest
     [TestMethod]
     public void TestConnectSecurityCamera()
     {
-        // Arrange
         SecurityCamera device = new SecurityCamera();
-
-        // Act
+        
         device.IsConnected = true;
-
-        // Assert
+        
         Assert.IsTrue(device.IsConnected);
     }
     
     [TestMethod]
     public void TestAddNameToSecurityCamera()
     {
-        // Arrange
         SecurityCamera device = new SecurityCamera();
-
-        // Act
+        
         device.Name = "Camera 1";
-
-        // Assert
+        
         Assert.AreEqual("Camera 1", device.Name);
     }
     
     [TestMethod]
     public void TestAddModelToSecurityCamera()
     {
-        // Arrange
         SecurityCamera device = new SecurityCamera();
-
-        // Act
+        
         device.Model = 1345354616346;
-
-        // Assert
+        
         Assert.AreEqual(1345354616346, device.Model);
     }
     
     [TestMethod]
     public void TestAddPhotosToSecurityCamera()
     {
-        // Arrange
         SecurityCamera device = new SecurityCamera();
         List<string> photos = new List<string> { "https://example.com/photo1.jpg", "https://example.com/photo2.jpg" };
-
-        // Act
+        
         device.PhotoURLs = photos;
-
-        // Assert
+        
         Assert.AreEqual(photos, device.PhotoURLs);
     }
 
     [TestMethod]
     public void TestAddCompanyDataToSecurityCamera()
     {
-        // Arrange
         SecurityCamera device = new SecurityCamera();
         Company company = new Company()
         {
@@ -73,10 +60,8 @@ public class DevicesTest
             LogoURL = "https://example.com/logo.jpg"
         };
         
-        // Act
         device.Company = company;
         
-        // Assert
         Assert.AreEqual(company, device.Company);
         Assert.AreEqual("SecurityCameras & Co.", device.Company.Name);
         Assert.AreEqual("123456789", device.Company.RUT);
@@ -86,14 +71,11 @@ public class DevicesTest
     [TestMethod]
     public void TestAddDescriptionToSecurityCamera()
     {
-        // Arrange
         SecurityCamera device = new SecurityCamera();
         string description = "This is a security camera";
-
-        // Act
+        
         device.Description = description;
-
-        // Assert
+        
         Assert.AreEqual(description, device.Description);
     }
     
