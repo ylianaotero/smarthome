8c5d3ff96d23deda0e3ff4dfb41c1c46617f1f87
Yliana Otero <ylianaotero@gmail.com>
2024-09-12 22:10:04 -0300

    add: WindowSensor implements IDevice - REFACTOR

--- SmartHome/Domain/WindowSensor.cs
+++ SmartHome/Domain/WindowSensor.cs
@@ -1,3 +1,5 @@
+using IDomain;
+
 namespace Domain;
 
 public enum WindowSensorFunctionality
@@ -5,13 +7,13 @@ public enum WindowSensorFunctionality
     OpenClosed
 }
 
-public class WindowSensor
+public class WindowSensor : IDevice
 {
     public string Name { get; set; }
     public long Model { get; set; }
     public string Description { get; set; }
     public List<string> PhotoURLs { get; set; }
-    public Company Company { get; set; }
+    public ICompany Company { get; set; }
     public bool IsConnected { get; set; }
     public List<WindowSensorFunctionality> Functionalities { get; set; }
 }
