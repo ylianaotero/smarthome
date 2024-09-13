7d2337df0d02488ae45b496791ff18b33c104181
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-09-12 23:37:29 -0300

    [GREEN] user validator email

--- SmartHome/Domain/UserValidator.cs
+++ SmartHome/Domain/UserValidator.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using IDomain;
 
 namespace Domain;
@@ -12,6 +13,16 @@ public class UserValidator
     public bool ValidateSurname(string surname)
     {
         return !IsNull(surname);
+    }
+    
+    public bool ValidateEmail(string email)
+    {
+        string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+        if (IsNull(email) || !Regex.IsMatch(email, pattern))
+        {
+            return false;
+        }
+        return true;
     }
 
     private bool IsNull(string text)
