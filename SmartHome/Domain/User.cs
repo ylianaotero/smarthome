580bd13416aed202e83d6c7744557978daa12eb2
Angelina Maverino <angelinamaverino16@gmail.com>
2024-09-13 17:25:31 -0300

    [GREEN] validate user name

--- SmartHome/Domain/User.cs
+++ SmartHome/Domain/User.cs
@@ -5,15 +5,27 @@ namespace Domain;
 
 public class User
 {
-    public string Name { get; set; }
+    private string _name; 
     public string Surname { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public List<IRole> Roles { get; set; }
 
+    private IUserValidator _validator; 
+    
+    public string Name { 
+        get => _name;
+        set
+        {
+            _validator.ValidateName(value);
+            _name = value; 
+        } 
+    }
+
     public User()
     {
         Roles = new List<IRole>();
+        _validator = new UserValidator(); 
     }
 
     public void AddRole(IRole role)
