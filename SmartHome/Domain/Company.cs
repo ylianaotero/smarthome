3fafd9a7cedea8a193ca1ed054142d96f35c618d
Yliana Otero <ylianaotero@gmail.com>
2024-09-12 23:06:54 -0300

    add: Id property in Company - GREEN

--- SmartHome/Domain/Company.cs
+++ SmartHome/Domain/Company.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using IDomain;
 
 namespace Domain;
 
 public class Company : ICompany
 {
+    [Key]
+    public long Id { get; set; }
     public string Name { get; set; }
     public string RUT { get; set; }
     public string LogoURL { get; set; }
@@ -11,6 +14,7 @@ public class Company : ICompany
     public override bool Equals(object? obj)
     {
         return obj is Company company &&
+               Id == company.Id &&
                Name == company.Name &&
                RUT == company.RUT &&
                LogoURL == company.LogoURL;
