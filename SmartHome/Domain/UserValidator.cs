9ef037133238e5abbfe4f829ee779b2a9a48ff2f
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-09-12 23:31:59 -0300

    [GREEN] user validator name

--- SmartHome/Domain/UserValidator.cs
+++ SmartHome/Domain/UserValidator.cs
@@ -4,4 +4,13 @@ namespace Domain;
 
 public class UserValidator
 {
+    public bool ValidateName(string name)
+    {
+        return !IsNull(name);
+    }
+
+    private bool IsNull(string text)
+    {
+        return string.IsNullOrEmpty(text);
+    }
 }
