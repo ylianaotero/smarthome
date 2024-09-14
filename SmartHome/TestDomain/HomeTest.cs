d0f708946d417cf4597640490e64a3ae1ae7b713
Angelina Maverino <angelinamaverino16@gmail.com>
2024-09-13 21:26:01 -0300

    [RED] find member

--- SmartHome/TestDomain/HomeTest.cs
+++ SmartHome/TestDomain/HomeTest.cs
@@ -5,30 +5,40 @@ namespace TestDomain;
 [TestClass]
 public class HomeTest
 {
-    private string _street = "Calle 3";
-    private int _doorNumber = 0;
-    private int _latitude = 23;
-    private int _longitude = 34;
+    private const string Street = "Calle 3";
+    private const int DoorNumber = 0;
+    private const int Latitude = 23;
+    private const int Longitude = 34;
+    private const string Email1 = "juanperez@gmail.com"; 
+    private const string Email2 = "laurasanchez@gmail.com";
+
+    private (User, bool) _member; 
+    
+    private User _user; 
 
     private Home _home; 
     
     [TestInitialize]
     public void TestInitialize()
     {
-        _home = new Home(_street,_doorNumber,_latitude,_longitude);
+        _home = new Home(Street,DoorNumber,Latitude,Longitude);
+        _user = new User();
+        _user.Email = Email1;
+        _member = (_user, true); 
+
     }
     
     [TestMethod]
     public void CreateNewHome()
     {
         // Arrange
-        Home newHome = new Home(_street,_doorNumber,_latitude,_longitude);
+        Home newHome = new Home(Street,DoorNumber,Latitude,Longitude);
 
         // Assert
-        Assert.AreEqual(_street, newHome.Street);
-        Assert.AreEqual(_doorNumber, newHome.DoorNumber);
-        Assert.AreEqual(_latitude, newHome.Latitude);
-        Assert.AreEqual(_longitude, newHome.Longitude);
+        Assert.AreEqual(Street, newHome.Street);
+        Assert.AreEqual(DoorNumber, newHome.DoorNumber);
+        Assert.AreEqual(Latitude, newHome.Latitude);
+        Assert.AreEqual(Longitude, newHome.Longitude);
         Assert.AreEqual(0, newHome.Members.Count);
         Assert.AreEqual(0, newHome.Devices.Count);
     }
@@ -40,6 +50,21 @@ public class HomeTest
         (User, bool) input = (user, true);
         _home.AddMember(input); 
         Assert.AreEqual(1, _home.Members.Count());
+    }
+    
+    [TestMethod]
+    public void TestFindMember()
+    {
+        User user = new User();
+        user.Email = Email2; 
+        (User, bool) member2 = (user, true);
+        
+        _home.AddMember(_member); 
+        _home.AddMember(member2); 
+        
+        (User, bool) result = _home.FindMember(Email2); 
+        
+        Assert.AreEqual(member2, result);
     }
     
  
