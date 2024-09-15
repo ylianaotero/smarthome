34637d181c2ea6957b939f0a932831bc08d8ddf0
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-09-14 22:12:47 -0300

    add: members to house

--- SmartHome/Domain/Home.cs
+++ SmartHome/Domain/Home.cs
@@ -2,20 +2,26 @@ namespace Domain;
 
 public class Home
 {
+    private List<User> _members;
     public int Id { get; set; }
     public string Street { get; set; }
     public int DoorNumber { get; set; }
     public int Latitude { get; set; }
     public int Longitude { get; set; }
     
-    public List<User> Members { get; set; }
+    public List<User> Members
+    {
+        get => _members;
+        private init => _members = value;
+    }
     
     public Home(string street, int doorNumber,int latitude,int longitude)
     {
         Street = street;
         DoorNumber = doorNumber;
         Latitude = latitude;
-        Longitude = longitude; 
+        Longitude = longitude;
+        Members = new();
     }
 
 }
