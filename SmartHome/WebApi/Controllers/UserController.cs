7c884d63da3b65dcb5ae704a86017d9d2016737b
Angelina Maverino <angelinamaverino16@gmail.com>
2024-09-17 02:24:27 -0300

    [GREEN] try to create user that already exist test controller

--- SmartHome/WebApi/Controllers/UserController.cs
+++ SmartHome/WebApi/Controllers/UserController.cs
@@ -31,6 +31,10 @@ public class UserController : ControllerBase
         {
             return BadRequest(new { message = inputNotValid.Message });
         }
+        catch (ElementAlreadyExist elementAlreadyExist)
+        {
+            return Conflict(new { message = elementAlreadyExist.Message });
+        }
         catch (Exception exception)
         {
             return StatusCode(500, new { message = "An unexpected error occurred. Please try again later." });
