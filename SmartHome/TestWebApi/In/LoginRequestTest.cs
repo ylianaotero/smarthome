958b71b661a5f7a58486fa5e98149b459afd128a
Angelina Maverino <angelinamaverino16@gmail.com>
2024-09-17 04:53:17 -0300

    [GREEN] login request

--- SmartHome/TestWebApi/In/LoginRequestTest.cs
+++ SmartHome/TestWebApi/In/LoginRequestTest.cs
@@ -1,4 +1,5 @@
 using Domain;
+using Microsoft.AspNetCore.Identity.Data;
 
 namespace TestWebApi.In;
 
---
+++ SmartHome/WebApi/In/LoginRequest.cs
@@ -0,0 +1,7 @@
+namespace WebApi.In;
+
+public class LoginRequest
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
