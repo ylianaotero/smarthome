c4218d068a1f6e777657f72993e72312acc830a4
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-09-23 18:13:26 -0300

    new migration

---
+++ SmartHome/DataAccess/Migrations/20240923211117_HomeOwnerId.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class HomeOwnerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude",
+                table: "Home",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude",
+                table: "Home",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<long>(
+                name: "OwnerId",
+                table: "Home",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Home");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Longitude",
+                table: "Home",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Latitude",
+                table: "Home",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
