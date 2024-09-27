34c118653523f499c34332368f746e2757604fd2
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-09-27 00:10:32 -0300

    add companyService TestInitialize

--- SmartHome/TestService/CompanyServiceTest.cs
+++ SmartHome/TestService/CompanyServiceTest.cs
@@ -1,11 +1,27 @@
+using BusinessLogic;
 using Domain;
 using IBusinessLogic;
 using IDataAccess;
+using Moq;
 
 namespace TestService;
 
 [TestClass]
 public class CompanyServiceTest
 {
-    
+    private Mock<IRepository<Company>> _mockCompanyRepository;
+    private CompanyService _companyService;
+    private Company _company;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        CreateMockCompanyRepository();
+    }
+
+    private void CreateMockCompanyRepository()
+    {
+        _mockCompanyRepository = new Mock<IRepository<Company>>();
+        _companyService = new CompanyService(_mockCompanyRepository.Object);
+    }
 }
