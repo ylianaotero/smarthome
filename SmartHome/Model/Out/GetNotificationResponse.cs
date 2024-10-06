9e94228b8fb7aac808ac8347c6c8e5e6029461a5
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-10-06 18:20:35 -0300

    fix notification response

--- SmartHome/Model/Out/GetNotificationResponse.cs
+++ SmartHome/Model/Out/GetNotificationResponse.cs
@@ -1,9 +1,13 @@
+using Domain.Abstract;
 using Domain.Concrete;
 
 namespace Model.Out;
 
 public class GetNotificationResponse
 {
+    public Guid HardwareId { get; set; }
+    
+    public string DeviceKind { get; set; }
     public string Event { get;  set; }
     public DateTime CreatedAt { get;  set; }
     public bool Read { get;  set; }
@@ -11,6 +15,8 @@ public class GetNotificationResponse
     
     public GetNotificationResponse(Notification notification)
     {
+        HardwareId = notification.DeviceUnit.HardwareId;
+        DeviceKind = notification.DeviceUnit.Device.Kind;
         Event = notification.Event;
         CreatedAt = notification.CreatedAt;
         Read = notification.Read;
@@ -20,6 +26,8 @@ public class GetNotificationResponse
     public override bool Equals(object? obj)
     {
         return obj is GetNotificationResponse response &&
+               HardwareId == response.HardwareId &&
+               DeviceKind == response.DeviceKind &&
                Event == response.Event &&
                CreatedAt == response.CreatedAt &&
                Read == response.Read &&
