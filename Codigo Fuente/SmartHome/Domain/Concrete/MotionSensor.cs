3d7976eb460ce9ed1d883e8e021800aa6a440f0a
Yliana Otero <ylianaotero@gmail.com>
2024-10-20 18:20:19 -0300

    refactor: Id is Key - REFACTOR

--- Codigo Fuente/SmartHome/Domain/Concrete/MotionSensor.cs
+++ Codigo Fuente/SmartHome/Domain/Concrete/MotionSensor.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Concrete;
 
 public class MotionSensor
 {
+    [Key]
     public long Id { get; set; }
     
     public MotionSensor()
