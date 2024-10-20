2e738fa08cfd115592a4b01be0f16e830887556c
Yliana Otero <ylianaotero@gmail.com>
2024-10-20 18:16:24 -0300

    add: MotionSensor - GREEN

--- Codigo Fuente/SmartHome/Domain/Concrete/MotionSensor.cs
+++ Codigo Fuente/SmartHome/Domain/Concrete/MotionSensor.cs
@@ -1,19 +1,11 @@
-using Domain.Abstract;
-
 namespace Domain.Concrete;
 
-public enum MovementSensorFunctionality
+public class MotionSensor
 {
-    MotionDetection
-}
-
-public class MovementSensor : Device
-{
-    public List<MovementSensorFunctionality>? Functionalities { get; set; }
-    public override string Kind { get; set; }
+    public long Id { get; set; }
     
-    public MovementSensor()
+    public MotionSensor()
     {
-        Kind = "MovementSensor";
+        
     }
 }
