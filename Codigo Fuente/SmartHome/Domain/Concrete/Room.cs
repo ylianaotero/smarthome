38f418df78cd34a26866174e59ccf9e05ca5f060
Yliana Otero <ylianaotero@gmail.com>
2024-10-28 15:23:56 -0300

    REFACTOR

--- Codigo Fuente/SmartHome/Domain/Concrete/Room.cs
+++ Codigo Fuente/SmartHome/Domain/Concrete/Room.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Concrete;
 
 public class Room
 {
+    [Key]
     public long Id { get; set; }
     public string Name { get; set; }
 }
