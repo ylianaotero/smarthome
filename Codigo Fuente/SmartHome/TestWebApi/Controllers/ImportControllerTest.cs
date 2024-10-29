88e9ff695cac4aac8106a857c1c60cf3c9b2a5db
Angelina Maverino <angelinamaverino16@gmail.com>
2024-10-29 00:24:20 -0300

    [RED] import controller get importer names

---
+++ Codigo Fuente/SmartHome/TestWebApi/Controllers/ImportControllerTest.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace TestWebApi.Controllers;
+
+[TestClass]
+public class ImportControllerTest
+{
+    private string _directoryPath; 
+    private Mock<IImporter.IImporter> _mockImporter;
+
+    private List<string> _listImporters; 
+
+    private ImportController _importController; 
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _mockImporter = new Mock<IImporter.IImporter>(MockBehavior.Strict);
+        _directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..","..","..", "Imports", "JsonImporter");
+        _listImporters = new List<string>() { "JsonDeviceImporter.JsonDevicesImporter"};
+    }
+    
+    [TestMethod]
+    public void Test_GetImportersName_Okay()
+    {
+        _mockImporter
+            .Setup(x => x.GetImplementationsNames(_directoryPath))
+            .Returns(_listImporters);
+        
+        _importController = new ImportController(_mockImporter.Object);
+        
+        ObjectResult result = _importController.GetName(_directoryPath) as OkObjectResult;
+        List<string> response = result.Value as List<string>;
+        
+        _mockImporter.Verify();
+        
+        Assert.AreEqual(_listImporters, response);
+    }
+
+    
+    
+}
