74fe6d57d677c90dfeff375a2e6a48d543ed97e8
Yliana Otero <ylianaotero@gmail.com>
2024-11-02 22:14:07 -0300

    add: TestAddRoomToHomeOkStatusCode - RED

--- Codigo Fuente/SmartHome/TestWebApi/Controllers/HomesControllerTest.cs
+++ Codigo Fuente/SmartHome/TestWebApi/Controllers/HomesControllerTest.cs
@@ -603,6 +603,27 @@ public class HomesControllerTest
         Assert.AreEqual(NotFoundStatusCode, result.StatusCode);
     }
     
+    [TestMethod]
+    public void TestAddRoomToHomeOkStatusCode()
+    {
+        Room room = new Room()
+        {
+            Name = "Living Room"
+        };
+        
+        PostHomeRoomRequest request = new PostHomeRoomRequest()
+        {
+            Name = room.Name
+        };
+        
+        _mockHomeService.Setup(service => service.AddRoomToHome(It.IsAny<long>(), It.IsAny<Room>()));
+        _homeController = new HomeController(_mockHomeService.Object);
+        
+        ObjectResult? result = _homeController.AddRoomToHome(_home.Id,request) as OkObjectResult;
+        
+        Assert.AreEqual(OKStatusCode, result.StatusCode);
+    }
+    
     private GetHomeResponse DefaultHomeResponse()
     {
         return new GetHomeResponse(_home);
