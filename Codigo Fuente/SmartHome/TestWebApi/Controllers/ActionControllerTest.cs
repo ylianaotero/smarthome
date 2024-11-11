0a9c72ebddfb13b32bd92fb0c740e31c16e7c522
Yliana Otero <ylianaotero@gmail.com>
2024-11-11 15:15:52 -0300

    add: TestPostActionOkStatusCode - RED

---
+++ Codigo Fuente/SmartHome/TestWebApi/Controllers/ActionControllerTest.cs
@@ -0,0 +1,28 @@
+using IBusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace TestWebApi.Controllers;
+
+public class ActionControllerTest
+{
+    [TestMethod]
+    public void TestPostActionOkStatusCode()
+    {
+        Mock<IActionService> actionServiceMock = new Mock<IActionService>();
+        ActionController controller = new ActionController(actionServiceMock.Object);
+
+        PostActionRequest request = new PostActionRequest()
+        {
+            HomeId = 1,
+            HardwareId = Guid.NewGuid(),
+            Functionality = "OnOff"
+        };
+        
+        actionServiceMock.Setup(x => x.PostAction(request.HomeId, request.HardwareId, request.Functionality)).Returns("Success");
+
+        ObjectResult result = controller.PostAction(request) as OkObjectResult;
+        
+        Assert.AreEqual(200, result.StatusCode);
+    }
+}
